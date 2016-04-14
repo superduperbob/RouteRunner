45153e88b53e916b6d762c99ef1ed75d74d50319
superduperbob <h030847c@student.staffs.ac.uk>
2016-04-14 16:16:03 +0100

    Previous upload does not seem to have worked, trying again

--- proj.android/obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/HelloWorldScene.o.d
+++ proj.android/obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/HelloWorldScene.o.d
@@ -458,7 +458,8 @@ obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/HelloWorldScene.o: \
  C:/Cocos/frameworks/cocos2d-x-3.9/cocos/3d/prebuilt-mk/../../renderer/CCRenderer.h \
  C:/Cocos/frameworks/cocos2d-x-3.9/cocos/editor-support/cocostudio/prebuilt-mk/../../cocostudio/ActionTimeline/CCBoneNode.h \
  C:/Cocos/frameworks/cocos2d-x-3.9/cocos/editor-support/cocostudio/prebuilt-mk/../../cocostudio/ActionTimeline/CSLoader.h \
- jni/../../Classes/GameManager.h
+ jni/../../Classes/GameManager.h jni/../../Classes/LevelSelect.h \
+ C:/Cocos/frameworks/cocos2d-x-3.9/cocos/3d/prebuilt-mk/../../ui\CocosGUI.h
 
 jni/../../Classes/HelloWorldScene.h:
 
@@ -1377,3 +1378,7 @@ C:/Cocos/frameworks/cocos2d-x-3.9/cocos/editor-support/cocostudio/prebuilt-mk/..
 C:/Cocos/frameworks/cocos2d-x-3.9/cocos/editor-support/cocostudio/prebuilt-mk/../../cocostudio/ActionTimeline/CSLoader.h:
 
 jni/../../Classes/GameManager.h:
+
+jni/../../Classes/LevelSelect.h:
+
+C:/Cocos/frameworks/cocos2d-x-3.9/cocos/3d/prebuilt-mk/../../ui\CocosGUI.h:
